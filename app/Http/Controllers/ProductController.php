f2cc6e084789ff8c00fa20d3321d1243bb60ad5f
unknown <boodykhattab97@gmail.com>
2023-05-07 19:17:23 +0300

    crud done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,17 +3,63 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Product;
 class ProductController extends Controller
 {
+
+    public function show(Product $product){
+
+        return view ('products.show',compact('product'));
+    }
+
+    public function edit(Product $product){
+
+        return view ('products.edit',compact('product'));
+    }
+
+    public function update(Request $request , Product $product){
+        $request->validate([
+            'name' => 'required',
+            'detail' => 'required',
+        ]);
+        //create a new Product in the database
+         $product->update($request->all());
+        // redirect the user and send friendly message 
+      return redirect()->route('products.index')->with('success','Product updated succesfully');
+
+    }
     public function index()
     {
         // Logic to retrieve and display products
+        $products = Product::latest()->paginate(5);
+        return view('products.index',compact('products'))->with(request()->input('page'));
     }
 
+    public function store(Request $request){
+        // validate the input 
+        $request->validate([
+            'name' => 'required',
+            'detail' => 'required',
+        ]);
+        //create a new Product in the database
+         Product::create($request->all());
+        // redirect the user and send friendly message 
+      return redirect()->route('products.index')->with('success','Product added succesfully');
+    }
+     
+    // when i access /create it should go to create.blade.php
     public function create()
     {
         return view('products.create');
+        
     }
+
+    public function destroy(Product $product){
+        $product->delete();
+        return redirect()->route('products.index')->with('success','Product deleted succesfully');
+
+    }
+
+   
 
 }
